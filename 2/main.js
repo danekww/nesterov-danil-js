34dfbe6da4879231ca781ab06684d250b2af893b
DANILKATUT <_@gmail.com>
2025-12-25 01:44:54 +0300

    условные операторы по блок-схемам

--- 2/main.js
+++ 2/main.js
@@ -1,12 +1,32 @@
-let x = 10;
-let y = 5;
-let text = "Я учу JavaScript";
+// 1. Сравнение a и b
+let a = 7;
+let b = 5;
 
+if (a > b) {
+  console.log('Большее a');
+} else {
+  console.log('Большее b');
+}
 
-console.log(`x + y = ${x + y}`);
-console.log(`x - y = ${x - y}`);
-console.log(`x * y = ${x * y}`);
-console.log(`x / y = ${x / y}`);
+// 2. Проверка имени
+let name = prompt('Введите имя');
 
-console.log(`x + text = ${x + text}`);
-console.log(`x / text = ${x / text}`);
+if (name === '') {
+  console.log('Имя пустое');
+} else if (name.length < 2) {
+  console.log('Имя слишком короткое');
+} else if (name.length > 50) {
+  console.log('Имя слишком длинное');
+} else {
+  console.log(name);
+}
+
+// 3. Алгоритм без циклов
+a = 5; // ❗ НЕ let, а просто присваивание
+
+if (a > 10) {
+  console.log('a > 10');
+} else {
+  a = a + 1;
+  console.log('a увеличено на 1:', a);
+}
