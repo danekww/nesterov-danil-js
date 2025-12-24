37d51f09e3d756f968be21d01aa2a674cbe75f72
DANILKATUT <_@gmail.com>
2025-12-25 01:21:24 +0300

    variables and console output

--- 2/main.js
+++ 2/main.js
@@ -1,1 +1,12 @@
-console.log("Hello GitHub");
+let x = 10;
+let y = 5;
+let text = "Я учу JavaScript";
+
+
+console.log(`x + y = ${x + y}`);
+console.log(`x - y = ${x - y}`);
+console.log(`x * y = ${x * y}`);
+console.log(`x / y = ${x / y}`);
+
+console.log(`x + text = ${x + text}`);
+console.log(`x / text = ${x / text}`);
